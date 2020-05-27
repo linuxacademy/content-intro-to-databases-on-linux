ff0e2355e569c4279c0d28986ce1eddcb82d5c83
david thomas <david.thomas@linuxacademy.com>
2020-05-27 13:11:19 -0400

    creates table and inserts data for SQL demo

---
+++ demo.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users ( ID int, Name text, Department text, ExpenseCode int);
+INSERT INTO users (ID, Name, Department, ExpenseCode) values ('0', 'David', 'Engineering' ,'200');
+INSERT INTO users (ID, Name, Department, ExpenseCode) values ('1', 'Clay', 'HR' ,'100');
+INSERT INTO users (ID, Name, Department, ExpenseCode) values ('2', 'Sue', 'Sales' ,'300');
+INSERT INTO users (ID, Name, Department, ExpenseCode) values ('3', 'Betty', 'Marketing' ,'400');
